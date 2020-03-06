670e363b4820b13ce80bcbc105196e060d790f12
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2020-03-06 20:36:35 +0200

    Update install script

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y docker
-sudo usermod -a -G docker ec2-user
-curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo amazon-linux-extras install docker
 sudo service docker start
-sudo chkconfig docker on    
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
