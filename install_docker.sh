bd887baded0c2710e82e25d98ad71a7416de5eae
Emanuel Donalds <emanueldonalds@edu.novia.fi>
2020-03-06 19:54:21 +0200

    Update docker compose version

--- install_docker.sh
+++ install_docker.sh
@@ -2,7 +2,7 @@
 sudo yum update
 sudo yum install -y docker
 sudo usermod -a -G docker ec2-user
-sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker start
-sudo chkconfig docker on
+sudo chkconfig docker on    
